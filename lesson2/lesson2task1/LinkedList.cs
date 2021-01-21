849d8b9e9b3490423adb907f4465dc9b5ec2b55b
vasiliy.mayorov <vasyamayorov@yandex.ru>
2021-01-21 23:59:05 +0300

    Выполнено домашнее задание к уроку 2

---
+++ lesson2/lesson2task1/LinkedList.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson2task1
+{
+    class LinkedList : ILinkedList
+    {
+        public Node firstNode { get; set; }
+        public Node lastNode { get; set; }
+        private int Count = 0;
+
+        public void AddNode(int value)
+        {
+            var newNode = new Node { Value = value };
+
+            if (Count == 0)
+            {
+                firstNode = newNode;
+                lastNode = firstNode;
+                Count++;
+            }
+            else
+            {
+                lastNode.NextNode = newNode;
+                newNode.PrevNode = lastNode;
+                lastNode = newNode;
+                Count++;
+            }
+        }
+        // [node.Prev] [node] [*] [node.Next]
+        public void AddNodeAfter(Node node, int value)
+        {
+            var newNode = new Node { Value = value, PrevNode = node, NextNode = node.NextNode };
+            if (node.NextNode != null) // node != lastNode
+            {
+                node.NextNode.PrevNode = newNode;
+            }
+            node.NextNode = newNode;
+            if (node == lastNode)
+            {
+                lastNode = newNode;
+            }
+            Count++;
+        }
+
+        public Node FindNode(int searchValue)
+        {
+            var currentNode = firstNode;
+
+            while (currentNode != null)
+            {
+                if (currentNode.Value == searchValue)
+                {
+                    return currentNode;
+                }
+                currentNode = currentNode.NextNode;
+            }
+            return null;
+        }
+
+        public int GetCount()
+        {
+            return Count;
+        }
+
+        public void RemoveNode(int index)
+        {
+            int currentIndex = 0;
+            var currentNode = firstNode;
+            while (currentNode != null)
+            {
+                if (currentIndex == index)
+                {
+                    RemoveNode(currentNode);
+                }
+                currentNode = currentNode.NextNode;
+                currentIndex++;
+            }
+        }
+
+        public void RemoveNode(Node node)
+        {
+
+            if (node == firstNode)
+            {
+                if (node != lastNode)
+                {
+                    node.NextNode.PrevNode = null;
+                }
+
+                firstNode = node.NextNode;
+            }
+            else if (node == lastNode)
+            {
+                node.PrevNode.NextNode = null;
+                lastNode = node.PrevNode;
+            }
+            else
+            {
+                node.PrevNode.NextNode = node.NextNode;
+                node.NextNode.PrevNode = node.PrevNode;
+            }
+            Count--;
+        }
+    }
+}
---
+++ lesson2/lesson2task2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson2task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 10, 2, 4, 8, 22, 11, 6, 1 };
+            int searchResult = BinarySearch(array, 1);
+            if (searchResult == -1)
+            {
+                Console.WriteLine("Такого элемента нет в массиве");
+            }
+            else
+            {
+                Console.WriteLine("Элемент найден");
+            }
+            Console.ReadKey();
+        }
+
+        static int BinarySearch(int[] inputArray, int searchValue) // O(n*logn) + O(logn) = O(n+1) * O(logn) = O (n log n по основанию 2)
+        {
+            Array.Sort(inputArray); // O(n * log n)
+            int minIndex = 0; // O(1)
+            int maxIndex = inputArray.Length - 1; // O(1)
+            int x = 0;
+            while (minIndex <= maxIndex) // O (log n)
+            {
+                int midIndex = (minIndex + maxIndex) / 2; // O(1)
+                if (searchValue == inputArray[midIndex]) // O(1)
+                {
+                    return midIndex; // O(1)
+                }
+                else if (searchValue < inputArray[midIndex]) // O(1)
+                {
+                    maxIndex = midIndex - 1; // O(1)
+                }
+                else
+                {
+                    minIndex = midIndex + 1; // O(1)
+                }
+                x++;
+            }
+            return -1; // O(1)
+
+        }
+
+    }
+}
