fa5a160023b7978920d0cfc3e6b4bab6c308d533
vasiliy.mayorov <vasyamayorov@yandex.ru>
2021-02-01 00:55:51 +0300

    Выполнено задание 1

--- lesson4/lesson4task1/Program.cs
+++ lesson4/lesson4task1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BenchmarkDotNet.Running;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,7 @@ namespace lesson4task1
     {
         static void Main(string[] args)
         {
+            BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
         }
     }
 }
